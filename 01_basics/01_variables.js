5d44f0fcf80c88ddddca381dce94fd9b767f44e1
abhinandan-05 <159599917+abhinandan-05@users.noreply.github.com>
2024-02-11 03:21:34 +0000

    basics of variables in javaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 2345
+let accountEmail = "amritraj0511@gmail.com"
+var accountPassword = "123456"
+accountCity = "Bihar"
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail = "htpr@gmail.com"
+accountPassword = "45676"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
