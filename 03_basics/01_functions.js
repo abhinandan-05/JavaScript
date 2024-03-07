e8611a7e0f51683949b6c3c1cb5d4cefc02d97c0
Abhinandan Kumar <159599917+abhinandan-05@users.noreply.github.com>
2024-03-07 01:47:33 +0000

    function Object + Array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,35 @@ function loginuserMessage(username = "sam"){
 }
 
 // console.log(loginuserMessage("Abhinandan"))
-console.log(loginuserMessage("Abhi"));
+// console.log(loginuserMessage("Abhi"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500,1000))
+
+const user = {
+    username: "Abhi",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and pric is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "abhi",
+    price: 399
+})
+
+const myNewArray = [200, 300, 400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 600, 500]));
