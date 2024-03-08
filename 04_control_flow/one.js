52c3536987cc2b36a272f42c5de4ae89f47d00c9
Abhinandan Kumar <159599917+abhinandan-05@users.noreply.github.com>
2024-03-08 04:20:20 +0000

    Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+// if 
+const isUserloggedIn = true
+const temperature = 41
+
+
+// if (temperature === 40 ){
+//     console.log("less the 50");
+// } else {
+//     console.log("temperature is greater then 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than");
+// } else if (balance < 750) {
+//     console.log("less then 750");
+// } else if(balance < 900) {
+//     console.log("less then 750");
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+const isUserLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFormEmail = true
+
+if (isUserLoggedIn && debitcard && 2==3) {
+    console.log("Allow to by course");
+} 
+
+if (loggedInFromGoogle || loggedInFormEmail) {
+    console.log("User logged in");
+}
