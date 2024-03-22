0585bb03e0edea64407d1e4ffc4d6a97b7af0049
abhinandan-05 <abhicpr05@gmail.com>
2024-03-22 14:33:01 +0530

    project

--- 07_projects/4-GuessTheNumber/GuessTheNumber.js
+++ 07_projects/4-GuessTheNumber/GuessTheNumber.js
@@ -1,1 +1,9 @@
-console.log(Math.random() * 100 + 1);
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const remaining = document.querySelector(".lastResult");
+
