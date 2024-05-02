d42fdfc730da2d3d7533ecfda5d0f283e333d1c1
abhinandan-05 <abhicpr05@gmail.com>
2024-05-02 09:54:59 +0530

    Array

--- js-restart/9 Array and Loop/Chapter Code/Arrays.js
+++ js-restart/9 Array and Loop/Chapter Code/Arrays.js
@@ -1,16 +1,17 @@
 let primes = [2, 3, 5, 7, 11, 13, 17, 19];
-console.log(primes);
-console.log(primes[0]);
-console.log(primes[7]);
-console.log(primes.length);
-console.log(primes[7]);
-primes[primes.length - 1];
-console.log(primes[10]);
-primes[2] = 1;
-console.log(primes);
+// console.log(primes);
+// console.log(primes[0]);
+// console.log(primes[7]);
+// console.log(primes.length);
+// console.log(primes[7]);
+// primes[primes.length - 1];
+// console.log(primes[10]);
+// primes[2] = 1;
+// console.log(primes);
 
 let ticTacToe = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""]
 ];
+// console.log(ticTacToe);
