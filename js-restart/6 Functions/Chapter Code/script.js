b3c85dc3e3665df851ca59b389a7bfcd4c240624
abhinandan-05 <abhicpr05@gmail.com>
2024-05-11 13:25:16 +0530

    Arrow function

---
+++ js-restart/6 Functions/Chapter Code/script.js
@@ -0,0 +1,13 @@
+// Arrow Function
+
+const square = (number) => {
+  return number * number;
+};
+
+const square1 = (number) => number * number;
+
+const result = square(10);
+const result1 = square(10);
+
+console.log(result);
+console.log(result1);
