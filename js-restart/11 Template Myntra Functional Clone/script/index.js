33d430599354a6b10a02fa97e8c4ae2499428f02
abhinandan-05 <abhicpr05@gmail.com>
2024-05-15 06:53:46 +0530

    code improvement using function

--- js-restart/11 Template Myntra Functional Clone/script/index.js
+++ js-restart/11 Template Myntra Functional Clone/script/index.js
@@ -1,31 +1,29 @@
+function addToBag () {
+
+}
+
+function displayItemsOnHome
+
 let itemsContainerElement = document.querySelector(".items-container");
 
-let item = {
-  item_image: "images/1.jpg",
-  rating: {
-    stars: 4.5,
-    noOfReviews: 1400,
-  },
-  company_name: "Carlton London",
-  Item_name: "Rhodium-Plated CZ Floral Studs",
-  current_price: 606,
-  original_price: 1045,
-  discount_percentage: 42,
-};
+let innerHTML = "";
+items.forEach((item) => {
+  innerHTML += `
+  <div class="item-container">
+    <img class="item-image" src="${item.image}" alt="item image" />
+    <div class="rating">
+    ${item.rating.stars} ⭐ | ${item.rating.count}
+    </div>
+    <div class="company-name">${item.company}</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+      <span class="current-price">${item.current_price}</span>
+      <span class="original-price">${item.original_price}</span>
+      <span class="discount">${item.discount_percentage}</span>
+    </div>
+    <button class="btn-add-bag" onclick="addToBag">Add to Bag</button>
+  </div>
+  `;
+});
 
-itemsContainerElement.innerHTML = `
-<div class="item-container">
-  <img class="item-image" src="${item.item_image}" alt="item image" />
-  <div class="rating">
-  ${item.rating.stars} ⭐ | ${item.rating.noOfReviews}
-  </div>
-  <div class="company-name">${item.company_name}</div>
-  <div class="item-name">${item.Item_name}</div>
-  <div class="price">
-    <span class="current-price">${item.current_price}</span>
-    <span class="original-price">${item.original_price}</span>
-    <span class="discount">${item.discount_percentage}</span>
-  </div>
-  <button class="btn-add-bag">Add to Bag</button>
-</div>
-`;
+itemsContainerElement.innerHTML = innerHTML;
