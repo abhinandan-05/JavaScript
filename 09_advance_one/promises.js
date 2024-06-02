ea7a48922230887d18fd96c1e04d2455b58c2645
Abhinandan Kumar <159599917+abhinandan-05@users.noreply.github.com>
2024-06-02 01:50:15 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -4,8 +4,23 @@ const promiseOne = new Promise(function (resolve, reject) {
   setTimeout(function () {
     console.log("Async task is compelete");
   }, 1000);
+  resolve()
 });
 
 promiseOne.then(function () {
   console.log("Promise consumed");
 });
+
+
+new Promise(function(resolve, reject) {
+  setInterval(function(){
+    console.log("Async task 2");
+    resolve()
+  }, 1000)
+}).then(function(){
+  console.log("Async 2 resolved");
+}) 
+
+const promiseThree = new Promise(function(resolve, reject) {
+  
+})
