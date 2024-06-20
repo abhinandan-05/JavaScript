bd2c8b5056827d7ea27f57e8b137d65dbfd84cff
abhinandan-05 <abhicpr05@gmail.com>
2024-06-20 13:33:55 +0900

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,3 +1,4 @@
+/*
 const accountId = 2345
 let accountEmail = "amritraj0511@gmail.com"
 var accountPassword = "123456"
@@ -12,10 +13,16 @@ accountCity = "Bangaluru"
 
 console.log(accountId);
 
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
+
+// Prefer not to use var
+// because of issue in block scope and functional scope
+
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+*/
+
+const accountId = 123423;
+let accountEmail = "abhicpr@gmail.com"
+var account
